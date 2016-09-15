b90cd29c3e7f32a32fff3d404f030f2e086ffc6a
cuihujun <hujuncui@yahoo.com>
2016-09-14 23:02:35 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by aaa on 9/14/16.
+//  Copyright © 2016 aaa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
