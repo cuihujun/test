2f0b9ad52b9250bb3fd739bb70b8ebb6f328cc2d
cuihujun <hujuncui@yahoo.com>
2016-10-04 22:27:25 -0400

    update linked list

--- test/SinglyLinkedList.cpp
+++ test/SinglyLinkedList.cpp
@@ -1,157 +1,510 @@
+
+
+#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-class Node{
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
+    if ( !v.empty() ) {
+        out << '[';
+        std::copy (v.begin(), v.end(), std::ostream_iterator<T>(out, " "));
+        out << "\b\b]";
+    }
+    return out;
+}
+
+class LinkedListNode
+{
 public:
-    Node() : value(0), next(NULL) {}
-    Node( int theValue, Node *theNext ) : value(theValue), next(theNext){}
-    int value;
-    Node *next;
+    int val;
+    LinkedListNode *next;
+    LinkedListNode(int node_value)
+    {
+        this->val = node_value;
+        this->next = NULL;
+    }
 };
 
-class LinkedList{
+class SinglyLinkedList
+{
 public:
-    LinkedList( );
-    ~LinkedList( );
-    void push_front( int value );
-    void push_back( int value );
-    void delete_front( );
-    void show_element( );
 
-    void BubbleSortVer1( );
-    void BubbleSortVer2( );
+    SinglyLinkedList( );
+    ~SinglyLinkedList( );
+    
+    LinkedListNode* getHead(){return head;}
+    void setHead(LinkedListNode* node){head = node;}
+    LinkedListNode* getTail(){return tail;}
+    void insertHead(LinkedListNode* newNode);
+    void insertTail(LinkedListNode* newNode);
+    void insertAfter(LinkedListNode *preNode, LinkedListNode* newNode);
+    
+    void removeHead();
+    void removeTail();
+    void removeAfter(LinkedListNode *preNode);
+    void removeNode(LinkedListNode *node);
+    
+    bool checkHeadValueEqualTo(int val);
+    LinkedListNode* searchNodeByVal_ReturnCurrNode(int val);
+    LinkedListNode* searchNodeByVal_ReturnPreNode(int val);
+    vector<LinkedListNode*> searchNodesByVal_ReturnVecPreNodes(int x);
+    LinkedListNode* searchNode_ReturnPreNode(LinkedListNode* node);
+    LinkedListNode* reverseKGroup(LinkedListNode *head, int k);
+    
+    // NOTE: Print the Node;
+    void printList();
+
 private:
-    Node* head;
-
-    Node* swap( Node*, Node* );
+    LinkedListNode* reverse(LinkedListNode *p1, LinkedListNode *p2);
+    
+    LinkedListNode* head;
+    LinkedListNode* tail;
 };
 
-LinkedList::LinkedList( ) : head(NULL){
 
-}
-LinkedList::~LinkedList( ){
-    if( head )
-        delete_front( );
-}
+#pragma mark - CONSTRUCTOR & DESTRUCTOR
+SinglyLinkedList::SinglyLinkedList( ) : head(NULL){}
+SinglyLinkedList::~SinglyLinkedList( ) {}
 
-void LinkedList::push_front( int value ){
-    Node *newNode = new Node(value, head);
-    head = newNode;
-}
-
-void LinkedList::push_back( int value ){
-    if( !head )
-        push_front( value );
-    else{
-        Node *current = head;
-        while( current->next != NULL )
-            current = current->next;
-        Node *newNode = new Node(value, NULL);
-        current->next = newNode;
-    }
-}
-
-void LinkedList::delete_front( ){
-    if( !head )
+#pragma mark - INSERT NODE FUNCTIONS
+void SinglyLinkedList::insertHead(LinkedListNode* newNode)
+{
+    // NOTE: Check input value
+    if (newNode == NULL)
+    {
         return;
-    Node *deleteNode = head;
-    head = head->next;
-    delete deleteNode;
-}
-
-void LinkedList::show_element( ){
-    Node *current = head;
-    while( current ){
-        cout << current->value << " ";
-        current = current->next;
     }
-    cout << endl;
-}
-
-void LinkedList::BubbleSortVer1( ){
-    if( !head ) return;
-    bool change = true;
-    while( change ){ //round
-        Node *prev = NULL;
-        Node *current = head;
-        change = false;
-        while( current->next != NULL ){
-            if( current->value > current->next->value ){
-                current = swap(current, current->next);
-                change = true;
-                if( prev != NULL )
-                    // swap is unrelated to head, update prev' s next link
-                    prev->next = current;
-                else
-                    // swap is related to head, change head pointer
-                    head = current;
-            }
-            prev = current;
-            current = current->next;
+    else
+    {
+        if (head == NULL)
+        {
+            // NOTE: If the head is NULL, it become the head
+            newNode->next = NULL;
+            head = newNode;
+            tail = newNode;
         }
-        show_element( );
-    }
-}
-
-void LinkedList::BubbleSortVer2( ){
-    if( !head ) return;
-    bool change = true;
-    Node *prevHead = new Node(0, head);
-    while( change ){
-        change = false;
-        Node *prev = prevHead;
-        Node *current = prev->next;
-        while( current->next != NULL ){
-            if( current->value > current->next->value ){
-                prev->next = current = swap(current, current->next);
-                change = true;
-            }
-            prev = current;
-            current = current->next;
+        else
+        {
+            // NOTE: Insert it to head
+            newNode->next = head;
+            head = newNode;
         }
     }
-    head = prevHead->next;
-    delete prevHead;
 }
 
-Node* LinkedList::swap( Node* p, Node* q){
-    Node *temp = q->next;
-    q->next = p;
-    p->next = temp;
-    return q;
+void SinglyLinkedList::insertTail(LinkedListNode *newNode)
+{
+    // NOTE: Check input value
+    if (newNode == NULL)
+    {
+        return;
+    }
+    else
+    {
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            // NOTE: If the head is NULL, it become the head
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            // NOTE: Insert it after tail
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
 }
-int main( ){
-    LinkedList list;
-    list.push_back( 4 );
-    list.push_back( 7 );
-    list.push_back( 5 );
-    list.push_back( 1 );
-    list.push_back( 3 );
-    list.push_back( 2 );
-    list.push_front( 6 );
-    list.push_front( 0 );
-    list.show_element( );
 
-    list.delete_front( );
-    list.show_element( );
-
-    cout << "sorting list1: \n";
-    list.BubbleSortVer2( );
-    cout << "After sort: ";
-    list.show_element( );
-
-    LinkedList list2;
-    list2.push_back( 1 );
-    list2.push_back( 2 );
-    list2.push_back( 3 );
-    list2.push_back( 4 );
-    list2.push_back( 5 );
-    list2.push_back( 7 );
-    list2.push_back( 6 );
-
-    cout << "\nsorting list2: \n";
-    list2.BubbleSortVer2( );
-    cout << "After sort: ";
-    list2.show_element( );
-    return 0;
+void SinglyLinkedList::insertAfter(LinkedListNode *preNode, LinkedListNode *newNode)
+{
+    if (newNode == NULL)
+    {
+        return;
+    }
+    else
+    {
+        if (preNode == NULL)
+        {
+            insertHead(newNode);
+        }
+        else if (preNode == tail)
+        {
+            insertTail(newNode);
+        }
+        else
+        {
+            // NOTE: Insert newNode after preNode
+            LinkedListNode *next = preNode->next;
+            preNode->next = newNode;
+            newNode->next = next;
+        }
+    }
 }
+
+#pragma mark - REMOVE NODE FUNCTIONS
+void SinglyLinkedList::removeHead()
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    else
+    {
+        LinkedListNode* removedNode;
+        removedNode = head;
+        if (head == tail)
+        {
+            head = NULL;
+            tail = NULL;
+        }
+        else
+        {
+            head = head->next;
+        }
+        delete removedNode;
+        removedNode = NULL;
+    }
+}
+void SinglyLinkedList::removeTail()
+{
+    if (tail == NULL)
+    {
+        return;
+    }
+    else
+    {
+        LinkedListNode *removedNode;
+        removedNode = tail;
+        if (head == tail)
+        {
+            head = NULL;
+            tail = NULL;
+        }
+        else
+        {
+            LinkedListNode *previousToTail = head;
+            while (previousToTail->next != tail)
+            {
+                previousToTail = previousToTail->next;
+            }
+            tail = previousToTail;
+            tail->next = NULL;
+        }
+        delete removedNode;
+        removedNode = NULL;
+    }
+}
+
+void SinglyLinkedList::removeNode(LinkedListNode *currNode)
+{
+#if 0
+    if (currNode == NULL)
+        return;
+    
+    if(currNode == head)
+    {
+        removeHead();
+    }
+    else if(currNode == tail)
+    {
+        removeTail();
+    }
+    else
+    {
+        // NOTE: Re-Search the list to get the preNode
+        LinkedListNode* preNode = searchNode_ReturnPreNode(currNode);
+        removeAfter(preNode);
+    }
+    return;
+#else
+    
+    if (currNode == NULL)
+        return;
+    
+    if(currNode == head)
+    {
+        removeHead();
+    }
+    else if(currNode == tail)
+    {
+        removeTail();
+    }
+    else
+    {
+        LinkedListNode* next = currNode->next;
+        currNode->val = next->val;
+        currNode->next = next->next;
+        
+        delete next;
+        next = NULL;
+        return;
+    }
+#endif
+}
+
+void SinglyLinkedList::removeAfter(LinkedListNode *preNode)
+{
+    if (preNode == NULL)
+    {
+        return;
+    }
+    else if (preNode == tail)
+    {
+        return;
+    }
+    else if (preNode->next == tail)
+    {
+        LinkedListNode *removedNode = preNode->next;
+        tail = preNode;
+        tail->next = NULL;
+        delete removedNode;
+        removedNode = NULL;
+    }
+    else
+    {
+        if(preNode->next)
+        {
+            LinkedListNode *removedNode = preNode->next;
+            preNode->next = removedNode->next;
+            delete removedNode;
+            removedNode = NULL;
+        }
+        else
+        {
+            cerr << "[Error][removeAfter] preNode->next is NULL" << endl;
+        }
+    }
+}
+
+#pragma mark - K-Group Reverse NODE FUNCTIONS
+
+/*
+ 
+ SinglyLinkedList* list = new SinglyLinkedList();
+ 
+ vector<int> item = {1,2,3,4,5,6,7,8,9};
+ 
+ int _list_item;
+ int _list_i;
+ 
+ for(_list_i=0; _list_i<item.size(); _list_i++)
+ {
+ _list_item = item[_list_i];
+ LinkedListNode* newNode = new LinkedListNode(_list_item);
+ list->insertTail(newNode);
+ }
+ 
+ list->printList();  // OUTPUT: 1 2 3 4 5 6 7 8 9
+ LinkedListNode* head = list->reverseKGroup(list->getHead(), 3);
+ list->printList();  // OUTPUT: 3 2 1 6 5 4 9 8 7
+
+ */
+LinkedListNode* SinglyLinkedList::reverse(LinkedListNode *p1, LinkedListNode *p2)
+{
+    LinkedListNode *p1next = p1->next;
+    LinkedListNode *p2next = p2->next;
+    LinkedListNode *prev = p2next;
+    LinkedListNode *p = p1next;
+    
+    while (p != p2next)
+    {
+        LinkedListNode *temp = p ->next;
+        p->next = prev;
+        prev = p;
+        p = temp;
+    }
+    p1->next = p2;
+    return p1next;
+}
+
+LinkedListNode* SinglyLinkedList::reverseKGroup(LinkedListNode* head, int k)
+{
+    if (k == 1)
+    {
+        return head;
+    }
+    
+    LinkedListNode *dummy = new LinkedListNode(0);
+    dummy->next = head;
+    
+    LinkedListNode *p1 = dummy, *p2 = dummy;
+    
+    for (int i = 0; i < k; i++)
+    {
+        if (p2 == NULL)
+        {
+            break;
+        }
+        p2 = p2->next;
+    }
+    if (p2 == NULL)
+    {
+        return head;
+    }
+    
+    while (p2 != NULL)
+    {
+        p2 = reverse(p1, p2);
+        for (int i = 0; i < k; i++)
+        {
+            if (p2 == NULL)
+            {
+                break;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+    }
+    setHead(dummy->next);
+    
+    return dummy->next;
+}
+
+#pragma mark - SEARCH NODE FUNCTIONS
+
+bool SinglyLinkedList::checkHeadValueEqualTo(int val)
+{
+    if(head->val == val)
+        return true;
+    else
+        return false;
+}
+
+LinkedListNode* SinglyLinkedList::searchNode_ReturnPreNode(LinkedListNode* currNode)
+{
+    LinkedListNode* temp = head;
+    
+    if(currNode == head)
+    {
+        cerr << "[searchNode_ReturnPreNode] check head of node first" << endl;
+        return NULL;
+    }
+    
+    while(temp!=NULL&&temp->next!=NULL)
+    {
+        if(temp->next==currNode)
+            return temp;
+        
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+LinkedListNode* SinglyLinkedList::searchNodeByVal_ReturnCurrNode(int val)
+{
+    LinkedListNode* temp = head;
+    while(temp!=NULL)
+    {
+        if(temp->val==val)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+LinkedListNode* SinglyLinkedList::searchNodeByVal_ReturnPreNode(int val)
+{
+    LinkedListNode* temp = head;
+    if(checkHeadValueEqualTo(val))
+    {
+        cerr << "[searchNodeByVal_ReturnPreNode] check head of node first" << endl;
+        return NULL;
+    }
+    
+    while(temp!=NULL)
+    {
+        // NOTE: Check value here
+        if(temp->next && temp->next->val==val)
+            return temp;
+        
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+vector<LinkedListNode*> SinglyLinkedList::searchNodesByVal_ReturnVecPreNodes(int x)
+{
+    vector<LinkedListNode*> res = {};
+    LinkedListNode* temp = head;
+    
+    if(temp->val == x)
+    {
+        cout << "[searchEqualX_ReturnPreNode] the value is at the Head of list" << endl;
+        return {};
+    }
+    
+    while(temp!=NULL)
+    {
+        if(temp->next && temp->next->val == x)
+            res.push_back(temp);
+        temp = temp->next;
+    }
+    return res;
+}
+
+#pragma mark - PRINT NODE FUNCTIONS
+void SinglyLinkedList::printList()
+{
+    LinkedListNode* temp=head;
+    
+    cout <<"PRINT OUT THE NODE VALUES"<<endl;
+    while(temp!=NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout <<endl;
+}
+
+#pragma mark - MAIN FUNCTION
+int main()
+{
+    
+    SinglyLinkedList* list = new SinglyLinkedList();
+    
+    vector<int> item = {1,2,3,4,5,6,7,8,9};
+    
+    int _list_item;
+    int _list_i;
+    
+    for(_list_i=0; _list_i<item.size(); _list_i++)
+    {
+        _list_item = item[_list_i];
+        LinkedListNode* newNode = new LinkedListNode(_list_item);
+        list->insertTail(newNode);
+    }
+    
+    list->printList();
+    LinkedListNode* head = list->reverseKGroup(list->getHead(), 3);
+    list->printList();
+    
+    return 0 ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
