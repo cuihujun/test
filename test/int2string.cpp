070fd3b6e6d1fa6910a385977ad46b70d3c7168d
cuihujun <hujuncui@yahoo.com>
2016-10-09 12:35:11 -0400

    add sprintf method

--- test/int2string.cpp
+++ test/int2string.cpp
@@ -4,13 +4,21 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     int num = 123;
     char buf[ 5];
 
     // convert 123 to string [buf]
-    itoa(num, buf, 10 );
+    
+
+    // Method 1
+    //itoa(num, buf, 10 );
+
+    // Method 2
+    sprintf(buf, "%d", num);
 
     // print our string
     printf( "%s\n", buf);
