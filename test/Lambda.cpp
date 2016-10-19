c1a81e6e9d193aae7424d47fc0a82015b7936ed6
cuihujun <hujuncui@yahoo.com>
2016-10-19 13:37:04 -0400

    Lambda functions with symbols

--- test/Lambda.cpp
+++ test/Lambda.cpp
@@ -22,7 +22,15 @@ int main()
 	};
 	helloWorldNum(4, 5);
 
-	auto helloWorldPara = [i, j](int a, int b)
+	// auto helloWorldPara = [i, j](int a, int b)
+	// {
+	// 	cout << "a+b= "<< a+b << endl;
+	// 	cout << "i: " << i << endl; // 10
+	// 	cout << "i: " << j << endl; // 11
+	// };
+	// helloWorldPara(1, 2);
+
+	auto helloWorldPara = [=](int a, int b)
 	{
 		cout << "a+b= "<< a+b << endl;
 		cout << "i: " << i << endl; // 10
@@ -30,8 +38,20 @@ int main()
 	};
 	helloWorldPara(1, 2);
 
+	// auto helloWorldRef = [&i, &j]()
+	// {
+	// 	cout << "i: " << i << endl; // 10
+	// 	cout << "i: " << j << endl; // 11
 
-	auto helloWorldRef = [&i, &j]()
+	// 	/* the variable values will be changed*/
+	// 	i=100;
+	// 	j=101;
+	// };
+	// helloWorldRef();
+	// cout << i << endl;  // 100
+	// cout << j << endl;  // 101
+
+	auto helloWorldRef = [&]()
 	{
 		cout << "i: " << i << endl; // 10
 		cout << "i: " << j << endl; // 11
@@ -49,7 +69,17 @@ int main()
 }
 
 
-
+/*
+[&]{}; //ok: by-reference capture default
+[=]{}; //ok: by-copy capture default
+[&, i]{}; // ok: by-reference capture, except i is captured by copy
+[=, &i]{}; // ok: by-copy capture, except i is captured by reference
+[&, &i] {}; // error: by-reference capture when by-reference is the default
+[=, this] {}; // error: this when = is the default
+[=, *this]{}; // ok: captures the enclosing S2 by copy (C++17)
+[i, i] {}; // error: i repeated
+[this, *this] {}; // error: "this" repeated (C++17)
+*/
 
 
 
